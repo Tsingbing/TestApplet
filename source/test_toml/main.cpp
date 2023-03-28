e1492d0336b7c58f939de937ed58dc720815146b
xueqingbing <qingbing.xue@i-tage.com>
2023-03-28 09:46:37 +0800

    添加取得toml文件的相应接口

--- source/test_toml/main.cpp
+++ source/test_toml/main.cpp
@@ -1,33 +1,58 @@
 ﻿#include "toml11/toml.hpp"
-
-#include <filesystem>
+//#include <filesystem>
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
-int main() {
-  cout << "Hello World!" << endl;
+int main()
+{
+    //cout << "Hello World!" << endl;
 
-  if (!std::filesystem::exists("config.toml")) {
-    std::cout << "config.toml not exit" << endl;
+    //    if (!std::filesystem::exists("vehicle_fault.toml")) {
+    //        std::cout << "config.toml not exit" << endl;
+    //        return 0;
+    //    }
+
+    const auto data = toml::parse<toml::preserve_comments>("vehicle_fault.toml");
+
+    std::map<int, std::string> faultCodeAndName; //存储maincode和故障名
+
+    for (auto &it : data.as_table()) {
+        if (!it.second.is_table()) continue;
+
+        const auto &table = it.second.as_table();
+
+        int _maincode = toml::get<int>(table.find("mainCode")->second);
+
+        //const auto &comment = toml::find(data, it.first).comments().back(); //获取注释
+        faultCodeAndName.insert(std::make_pair(_maincode, it.first));
+    }
+
+    for (auto &it : faultCodeAndName) {
+        std::stringstream stream;
+        stream << std::hex << it.first;
+        std::string result(stream.str());
+        std::cout << result << ": " << it.second << std::endl;
+    }
+
+    //获取某个字段的注释
+    std::string faultName = faultCodeAndName.at(0x340001c5);
+    const auto &comment = toml::find(data, faultName).comments().back();
+    const auto &fault_level = toml::find(data, faultName).at("fault_level").at(1);
+    std::cout << comment << ":" << fault_level << std::endl;
+
+    //    const auto aot2 = toml::find<toml::table>(data, "AirFilterBlock_5");
+
+    //    std::cout << aot2.at("fault_level").at(1) << endl;
+    //std::cout << toml::find<int>(i, "mainCode") << endl;
+
+    //输出toml所有的[]表头
+    //  for (auto &i : data) {
+    //    std::cout << i.key() << endl;
+    //  }
+    // const auto title = toml::find<std::string>(data, "title");
+    // std::cout << "the title is " << title << std::endl;
     return 0;
-  }
-
-  //  const auto data = toml::parse("config.toml");
-  //  const auto aot2 = toml::find<std::vector<toml::table>>(data, "AID");
-  //  std::cout << data << std::endl;
-
-  //  for (auto &i : aot2) {
-
-  //    std::cout << i.at("fault_level").at(1) << endl;
-  //    // std::cout << toml::find<int>(i, "mainCode") << endl;
-  //  }
-
-  //  //输出toml所有的[]表头
-  //  //  for (auto &i : data) {
-  //  //    std::cout << i.key() << endl;
-  //  //  }
-  //  // const auto title = toml::find<std::string>(data, "title");
-  //  // std::cout << "the title is " << title << std::endl;
-  return 0;
 }
